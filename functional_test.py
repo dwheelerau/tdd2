657dfd68ca8a861ac474000856dc2fd1b5ca8679
Dave Wheeler <dwheelerau@gmail.com>
2016-08-01 09:55:07 +1200

    made ft a class

--- functional_test.py
+++ functional_test.py
@@ -1,30 +1,45 @@
 from selenium import webdriver
+import unittest
+import time
 
-browser = webdriver.Firefox()
 
-# Edith has head about a cool online to-do app she checks it out
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# She notices the page title and header mention to-do lists.
-assert 'To-Do' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
 
-# She is invited to enter a list
+    def tearDown(self):
+        time.sleep(3)
+        self.browser.quit()
 
-# She types "buy peacock feathers" into a text box
+    def test_can_start_a_list_and_retrieve_it_latter(self):
+        # Edith has head about a cool online to-do app she checks it out
+        self.browser.get('http://localhost:8000')
 
-# When she hits enter the page updates and now th epage lists
-# 1: Buy peacock feathers as an item on the todo list.
+        # She notices the page title and header mention to-do lists.
+        assert 'To-Do' in self.browser.title
+        self.fail("Finish the tests!")
 
-# There is still a text box inviting her to add another item
-# She enter "use peacock feathers to make a fly"
+        # She is invited to enter a list
 
-# The page updates again and shows both items.
+        # She types "buy peacock feathers" into a text box
 
-# Edith wonders if the site will remember her list. She sees
-# It has generated a unique url for her
-# There is some explanation text that describes this.
+        # When she hits enter the page updates and now th epage lists
+        # 1: Buy peacock feathers as an item on the todo list.
 
-# She visits this url and her list is there.
+        # There is still a text box inviting her to add another item
+        # She enter "use peacock feathers to make a fly"
 
-# Satisfied she goes back to sleep.
-browser.quit()
+        # The page updates again and shows both items.
+
+        # Edith wonders if the site will remember her list. She sees
+        # It has generated a unique url for her
+        # There is some explanation text that describes this.
+
+        # She visits this url and her list is there.
+
+        # Satisfied she goes back to sleep.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
