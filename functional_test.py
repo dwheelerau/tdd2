eab432c5f15f3418d35c35b1e8f3eb1095a1425a
Dave Wheeler <dwheelerau@gmail.com>
2016-08-01 11:50:16 +1200

    about to refrac ft

--- functional_test.py
+++ functional_test.py
@@ -39,20 +39,27 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in the table"
-        )
+        self.assertIn('1: Buy peacock feathers',
+                      [row.text for row in rows])
         # There is still a text box inviting her to add another item
         # She enter "use peacock feathers to make a fly"
-        self.fail('finish the tests!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # The page updates again and shows both items.
-
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers',
+                      [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
         # Edith wonders if the site will remember her list. She sees
         # It has generated a unique url for her
         # There is some explanation text that describes this.
-
+        self.fail('finish the tests!')
         # She visits this url and her list is there.
 
         # Satisfied she goes back to sleep.
